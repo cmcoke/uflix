668da760a7af989e1426d31a6afed964731f19dc
cmcoke <minoru.cm@gmail.com>
2022-10-06 21:41:52 -0500

    the initial project setup.

---
+++ src/assets/index.js
@@ -0,0 +1,20 @@
+export { default as logo } from './logo.png';
+export { default as closeWhite } from './close-white.svg';
+export { default as closeBlack } from './close-black.svg';
+export { default as movie } from './movie.svg';
+export { default as menu } from './menu.svg';
+export { default as search } from './search.svg';
+export { default as searchLarge } from './search-lg.svg';
+export { default as trending } from './trending.svg';
+export { default as tv } from './tv.svg';
+export { default as home } from './home.svg';
+export { default as noImage } from './no-image-placeholder.png';
+export { default as star } from './star.png';
+export { default as popcorn } from './popcorn.png';
+export { default as globe } from './globe.png';
+export { default as play } from './play.svg';
+export { default as facebook } from './facebook.svg';
+export { default as twitter } from './twitter.svg';
+export { default as instagram } from './instagram.svg';
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        black: "#050607",
+        dimWhite: "rgba(255, 255, 255, 0.7)"
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
