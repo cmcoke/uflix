63c80cb72494774d8cff1557d0cda1eef74753cd
cmcoke <minoru.cm@gmail.com>
2022-10-06 22:21:08 -0500

    created the sidebar component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { SideBar, Home, Trending, TvShows, TvShowInformation, Movies, MovieInformation } from './';
+import { menu, closeBlack } from '../assets/index';
 
 const App = () => {
-  return (
-    <div>
 
-      <aside>
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => {
+    setToggle(prevState => !prevState);
+    document.querySelector("body").classList.toggle("active");
+  }
+
+  return (
+    <div className="bg-black font-poppins text-white flex w-full min-h-screen overflow-hidden">
+
+      <aside className={`border-r border-neutral-900 bg-black transform top-0 left-0 sm:left-[120px] w-[120px] fixed h-full overflow-auto ease-in-out transition-all duration-300 z-10 ${toggle ? 'translate-x-0' : '-translate-x-full'}`}>
         <SideBar />
       </aside>
 
-      <main>
+      <main className="flex-1 relative z-0 sm:pl-[7rem]">
+
+        {/* shows the hamburger menu icon only on mobile screens */}
+        <div className="ss:hidden absolute right-7 top-4 bg-white p-2 rounded-full">
+          <img src={toggle ? closeBlack : menu} alt={toggle ? 'close button' : 'menu button'} onClick={handleClick} />
+        </div>
+
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/trending' element={<Trending />} />
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom';
+import { logo, home, trending, movie, tv, search } from '../assets/index';
 
 const SideBar = () => {
   return (
-    <div>Side Bar</div>
+    <div className='min-h-screen flex flex-col items-center justify-around sm:justify-between xs:py-3 sm:py-9 md:py-11 ss:fixed top-0 left-2'>
+
+      <img src={logo} alt="logo" />
+
+      <ul className='flex flex-col gap-y-[3rem] ss:gap-y-[3.3rem] xs:my-11'>
+
+        <li onClick={() => { }}>
+          <NavLink to='/' end className={({ isActive }) => isActive ? 'brightness-100 transition-all duration-[.40s] ease-in-out' : 'brightness-50 transition-all duration-[.40s] ease-in-out hover:brightness-100'} >
+            <img src={home} alt="home" />
+          </NavLink>
+        </li>
+
+        <li onClick={() => { }}>
+          <NavLink to='/trending' className={({ isActive }) => isActive ? 'brightness-100 transition-all duration-[.40s] ease-in-out' : 'brightness-50 transition-all duration-[.40s] ease-in-out hover:brightness-100'}>
+            <img src={trending} alt="trending" />
+          </NavLink>
+        </li>
+
+        <li onClick={() => { }}>
+          <NavLink to='/tv-shows/genre/:name' className={({ isActive }) => isActive ? 'brightness-100 transition-all duration-[.40s] ease-in-out' : 'brightness-50 transition-all duration-[.40s] ease-in-out hover:brightness-100'}>
+            <img src={tv} alt="tv shows" />
+          </NavLink>
+        </li>
+
+        <li onClick={() => { }}>
+          <NavLink to='/movies/genre/:name' className={({ isActive }) => isActive ? 'brightness-100 transition-all duration-[.40s] ease-in-out' : 'brightness-50 transition-all duration-[.40s] ease-in-out hover:brightness-100'}>
+            <img src={movie} alt="movies" />
+          </NavLink>
+        </li>
+
+        <li onClick={() => { }}>
+          <img src={search} alt="search" className={'brightness-50 transition-all duration-[.40s] ease-in-out hover:brightness-100 cursor-pointer'} />
+        </li>
+
+      </ul>
+
+      <p className='font-poppins text-dimWhite text-center text-sm'>Created By <a href="https://charlescoke.com/" target="_blank" rel="noreferrer" className='transition-all duration-[.40s] ease-in-out text-dimWhite hover:text-blue-600'>C.Coke</a> </p>
+
+    </div>
   )
 }
 
